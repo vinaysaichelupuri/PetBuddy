ff853fa3bada1257fc8757ce082c810583b05331
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2024-12-06 16:16:25 +0530

    Update th test code according to password encryption

--- tests/Login.test.tsx
+++ tests/Login.test.tsx
@@ -59,7 +59,7 @@ describe('Login Screen', () => {
     await waitFor(() => {
       expect(axios.post).toHaveBeenCalledWith('http://localhost:5001/api/login', {
         username: '',
-        password: '',
+        password: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
       });
       expect(mockNavigate).toHaveBeenCalledWith('Home');
     });
